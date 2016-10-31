7142bc85105e6e4af6113306644720afd7d86946
gwcmk <gregwcmkim@gmail.com>
2016-10-31 15:23:23 -0500

    Added Treant.js library

--- server/config/security.js
+++ server/config/security.js
@@ -1,4 +1,5 @@
 export default function (BrowserPolicy) {
+  BrowserPolicy.content.allowEval(); // TODO: remove this for prod
   BrowserPolicy.content.allowOriginForAll("*.googleapis.com");
   BrowserPolicy.content.allowOriginForAll("*.gstatic.com");
   BrowserPolicy.content.allowOriginForAll("*.bootstrapcdn.com");
